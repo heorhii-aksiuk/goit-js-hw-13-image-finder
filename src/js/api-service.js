fe0f42b59538b1780a6dc0d7faaac079b82f0c77
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-10-25 18:06:36 +0300

    Создание класса с логикой работы с запросами API

---
+++ src/js/api-service.js
@@ -0,0 +1,52 @@
+const BASE_URL = 'https://pixabay.com/api';
+const API_KEY = '24011643-a99499b85595b827654661749';
+
+export default class ApiService {
+  constructor() {
+    this._searchQuery = '';
+    this._page = 1;
+  }
+
+  fetchImages() {
+    const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
+    
+    return fetch(url).then(response => response.json()).then( ({hits}) => hits)
+  }
+
+  get searchQuery() {
+    return this._searchQuery;
+  }
+
+  set searchQuery(newQuery) {
+    this._searchQuery = newQuery;
+  }
+
+  nextPage() {
+    this._page += 1;
+  }
+
+  resetPage() {
+    this._page = 1;
+  }
+}
+
+//tests
+
+const apiService = new ApiService();
+
+apiService.fetchImages();
+apiService.searchQuery = 'cat';
+console.log(apiService.searchQuery);
+
+apiService.nextPage()
+apiService.nextPage();
+// apiService.resetPage();
+
+
+
+console.log(apiService._page);
+console.log();
+console.log();
+console.log();
+console.log();
+
