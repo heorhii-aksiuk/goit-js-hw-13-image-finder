89e299405c4f62fdca6fb5d1db97f3fea950b72f
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-10-25 23:33:33 +0300

    Реализация бесконечной прокрутки

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import ApiService from './js/api-service';
 import refs from './js/refs';
 import cardTemplate from './templates/image-card.hbs';
 
-const { inputEl, gallerySectionEl, buttonEl, listEl } = refs;
+const { bodyEl, inputEl, gallerySectionEl, listEl } = refs;
 listEl.classList.add('gallery');
 
 const apiService = new ApiService();
@@ -14,25 +14,30 @@ const apiService = new ApiService();
 /*  */
 
 inputEl.addEventListener('input', debounce(onInputChange, 1000));
-buttonEl.addEventListener('click', onButtonClick)
+window.addEventListener('scroll', onScroll)
 
 
 function onInputChange(event) {
   event.preventDefault();
-
+  
   apiService.searchQuery = inputEl.value.trim();
   
   if (!apiService.searchQuery.match(/^[a-zA-Z_ ]*$/) || apiService.searchQuery === '') return;
-
+  
   gallerySectionEl.appendChild(listEl);
   listEl.innerHTML = '';
   apiService.resetPage()
   createMarkup()
-  }
+}
 
-function onButtonClick() {
-  apiService.nextPage();
-  createMarkup()
+function onScroll() {
+  let contentHeight = bodyEl.offsetHeight;
+  let currentPosition = window.pageYOffset + window.innerHeight;
+
+  if (currentPosition >= contentHeight) {
+    apiService.nextPage();
+    createMarkup();
+  }
 }
   
 function createMarkup() {
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,6 +1,6 @@
 export default {
+  bodyEl: document.querySelector('body'),
   inputEl: document.querySelector('input'),
   gallerySectionEl: document.querySelector('.images-gallery-js'),
-  buttonEl: document.querySelector('.load-more-btn'),
   listEl: document.createElement('ul'),
 };
