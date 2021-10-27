895d91ac6ba8e8dee79f5383e455b85977dcae81
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-10-27 22:13:07 +0300

    Рефакторинг js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
-//Project styles
 import './sass/main.scss';
-//Libs
 import debounce from 'lodash.debounce';
-//Project files
 import ApiService from './js/api-service';
-import refs from './js/refs';
+import { inputEl, gallerySectionEl, listEl, sentinelEl, errorTextEl } from './js/refs';
 import cardTemplate from './templates/image-card.hbs';
-//Other setup
-const { inputEl, gallerySectionEl, listEl, sentinelEl, errorTextEl } = refs;
-listEl.classList.add('gallery');
 
 const apiService = new ApiService();
+listEl.classList.add('gallery');
 
 /*  */
 
@@ -20,7 +15,7 @@ observer.observe(sentinelEl);
 
 function onInputChange(event) {
   event.preventDefault();
-  listEl.remove()
+  listEl.remove();
 
   apiService.searchQuery = inputEl.value.trim();
 
@@ -33,29 +28,23 @@ function onInputChange(event) {
 }
 
 async function createMarkup() {
-  errorTextEl.remove()
+  const nothingFound = 'По вашему запросу ничего не найдено';
+  errorTextEl.remove();
+
   try {
     const apiResponse = await apiService.fetchImages();
 
     if (apiResponse.length !== 0) {
-      const markup = listEl.insertAdjacentHTML('beforeend', cardTemplate(apiResponse));
-      return markup;
-
+      listEl.insertAdjacentHTML('beforeend', cardTemplate(apiResponse));
     } else if (apiService.total >= 1) {
       return;
-
     } else {
-      throw Error('По вашему запросу ничего не найдено');
+      throw Error(nothingFound);
     }
-
   } catch (error) {
     listEl.remove();
     gallerySectionEl.appendChild(errorTextEl);
-    if (error.message === 'По вашему запросу ничего не найдено') {
-      errorTextEl.innerHTML = error.message;
-    } else {
-      errorTextEl.innerHTML = `Что-то пошло не так... ${error.message}.`;
-    }
+    createErrorMessage(error, nothingFound);
   }
 }
 
@@ -67,3 +56,10 @@ function onEntry(entries) {
     }
   });
 }
+
+function createErrorMessage(error, nothingFound) {
+  const errorText = 'Что-то пошло не так...'
+  errorTextEl.innerHTML =
+    error.message === nothingFound ? error.message : `${errorText} ${error.message}`;
+}
+
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,7 +1,9 @@
-export default {
+const refs = {
   inputEl: document.querySelector('input'),
   gallerySectionEl: document.querySelector('.images-gallery-js'),
   listEl: document.createElement('ul'),
   errorTextEl: document.createElement('p'),
   sentinelEl: document.querySelector('.sentinel'),
 };
+
+export const { inputEl, gallerySectionEl, listEl, sentinelEl, errorTextEl } = refs;
